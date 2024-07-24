129bd24f2fb275882ee9ca8823be4f8d58193a97
Dave May <echo.dave@protonmail.com>
2024-07-23 20:56:50 -0400

    fix: surround mappings, closing-pairs ignore alpha
    closing pairs shouldn't be added when following character is
    aphanumerica

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,6 +1,23 @@
 return {
 	"kylechui/nvim-surround",
-	event = { "BufReadPre", "BufNewFile" },
+	-- event = { "BufReadPre", "BufNewFile" },
+	event = "VeryLazy",
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	config = true,
+	config = function()
+		require("nvim-surround").setup({
+			keymaps = {
+				insert = "<C-g>s",
+				insert_line = "<C-g>S",
+				normal = "ys",
+				normal_cur = "yss",
+				normal_line = "yS",
+				normal_cur_line = "ySS",
+				visual = "S",
+				visual_line = "gS",
+				delete = "ds",
+				change = "cs",
+				change_line = "cS",
+			},
+		})
+	end,
 }

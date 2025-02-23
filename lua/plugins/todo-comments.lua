a0db86ddc18691aa19de72a9c63fe91297fdbac8
Dave May <echo.dave@protonmail.com>
2025-02-23 17:57:55 -0500

    update add keymaps for snacks picker

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -18,4 +18,20 @@ return {
 
 		todo_comments.setup()
 	end,
+	keys = {
+		{
+			"<leader>st",
+			function()
+				Snacks.picker.todo_comments()
+			end,
+			desc = "Todo",
+		},
+		{
+			"<leader>sT",
+			function()
+				Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+			end,
+			desc = "Todo/Fix/Fixme",
+		},
+	},
 }

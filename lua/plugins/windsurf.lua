d5ac6204c7b2345730bb5ba2629c78bcfd429825
Dave May <echo.dave@protonmail.com>
2026-08-21 19:39:35 -0400

    fix codeium rebranded windsurf bought by devin

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,12 +1,12 @@
 return {
-	"Exafunction/codeium.vim",
+	"Exafunction/windsurf.vim",
 	event = "BufEnter",
 	config = function()
 		--  vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
 
-		vim.keymap.set("i", "<c-;>", function()
-			return vim.fn["codeium#CycleCompletions"](1)
-		end, { expr = true, silent = true })
+		-- vim.keymap.set("i", "<c-;>", function()
+		-- 	return vim.fn["codeium#CycleCompletions"](1)
+		-- end, { expr = true, silent = true })
 
 		vim.keymap.set("i", "<m-;>", "<cmd>CodeiumToggle<cr>", { silent = true })
 
@@ -14,14 +14,13 @@ return {
 			return vim.fn["codeium#Complete"]()
 		end, { expr = true, silent = true, noremap = true })
 
-		-- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
 		vim.keymap.set("i", "<c-x>", function()
 			return vim.fn["codeium#Clear"]()
 		end, { expr = true, silent = true })
 
 		-- Toggle Codeium <m-;>
 		-- Get Suggestions <m-p>
-		-- Cycle Suggestions <c-;>
+		-- Cycle Suggestions <m-]> <m-[>
 		-- Clear suggestions <c-x>
 
 		-- vim.g.codeium_enabled = false
